64e254f602c2f2f20827d52c98d9941c425664b9
Pranjal Jain <jainpranj@gmail.com>
2017-04-11 16:47:24 -0400

    pushing code to github with all emotions analysis

---
+++ FinalProject/Github-Data-Sentimental-Analysis-Joy/src/main/java/com/github/sentimentalanalysis/GithubSentimentalAnlysisMapper.java
@@ -0,0 +1,44 @@
+package com.github.sentimentalanalysis;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+
+public class GithubSentimentalAnlysisMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	public static String joyWordsRegex;
+
+
+	public IntWritable one = new IntWritable(1);
+	public static final String[] listOfEmotions = { "Anger", "Joy", "Amusement", "Surpise", "Issue/Bugs", "Swearing" };
+
+	protected void setup(Context context) throws IOException, InterruptedException {
+
+		// joy
+		joyWordsRegex = "(?i).*\\b(yes|yay|hallelujah|hurray|bingo|amused|cheerful|excited|glad|proud)\\b.*";
+
+
+
+
+	}
+
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		if (key.get() > 0) {
+	
+			String commitMessage = value.toString().substring(0, value.toString().lastIndexOf(',')).trim();
+			String language = value.toString().substring(value.toString().lastIndexOf(',') + 1).trim();
+
+			if (commitMessage.matches(joyWordsRegex)) {
+				context.write(new Text(language), one);
+			}
+		}
+
+	}
+
+
+
+}
